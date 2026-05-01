7625c84a0d8604a1a3823197168f96dcfae37589
Saeed Bohlooli <sbohlooli@keithsinger.onmicrosoft.com>
2026-05-01 06:19:55 +0000

    addingg from server

--- bin/run-trading-api-service.bat
+++ bin/run-trading-api-service.bat
@@ -8,16 +8,5 @@ set KEY_2=p110
 
 set CMD="python ..\trading_api\trading_api_service.py --portfolio-id %PORTFOLIO%"
 
-wmic process where "name='%PROGRAM%' and CommandLine like '%%%KEY_1%%%' and CommandLine like '%%%KEY_2%%%' " get ProcessId | findstr [0-9] >nul
-
-:: for debugging
-:: wmic process where "name='node.exe' " get ProcessId,CommandLine
-
-if %errorlevel%==0 (
-    echo We already running for %PROGRAM% %KEY_1% %KEY_2% %CMD%
-    echo === Running process ===
-    wmic process where "name='%PROGRAM%' and CommandLine like '%%%KEY_1%%%' and CommandLine like '%%%KEY_2%%%' " get ProcessId,CommandLine
-) else (
-    echo Starting program %CMD%
-    start cmd /k %CMD%
-)
+echo Starting program %CMD%
+start cmd /k %CMD%
